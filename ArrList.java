974005ff3303f9d7f974b3c86672f8628d7007ad
parkminhee-05 <62226042+parkminhee-05@users.noreply.github.com>
2020-04-14 02:50:47 +0900

    Add files via upload

---
+++ ArrList.java
@@ -0,0 +1,53 @@
+import java.util.NoSuchElementException;
+
+public class ArrList<E> {
+		private E a[];	//리스트의 항목들을 저장할 배열
+		private int size;	//리스트의 항목 수
+		public ArrList() {	//생성자
+			a = (E[]) new Object[1];	//최초로 1개의 원소를 가진 배열 생성
+			size = 0;	//항목 수를 0으로 초기화
+		}
+		public boolean isEmpty() {return size == 0;}
+		
+		public void insertLast(E newItem) {
+			if(size == a.length)
+				resize(2*a.length);
+			a[size++] = newItem;
+	}
+		
+		public void insert(E newItem, int k) {
+			if (size == a.length)
+				resize(2*a.length);
+			for(int i = size-1; i>=k; i--) a[i+1] = a[i];
+			a[k] = newItem;
+			size++;
+}
+		public E delete(int k) {
+			if (isEmpty()) throw new NoSuchElementException();
+			E item = a[k];
+			for (int i = k; i <size; i++) a[i] = a[i+1];
+			size--;
+			if(size > 0 && size == a.length/4)
+				resize(a.length/2);
+			return item;
+		}
+		public E peek(int k) {
+			if(isEmpty()) throw new NoSuchElementException();
+			return a[k];
+		}
+		private void resize(int newSize) {
+			Object[] t = new Object[newSize];
+			for(int i = 0; i < size; i++)
+				t[i] = a[i];
+			a = (E[]) t;
+		}
+		public void print() {
+			if(isEmpty())
+				System.out.print("배열이 비어있음.");
+			else
+				for(int i = 0; i < a.length; i++) System.out.print(a[i]+"\t ");
+			System.out.println();
+		}
+		
+}
+		
