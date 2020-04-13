eaac67f09dfbcc3c047f739201af563320062974
parkminhee-05 <62226042+parkminhee-05@users.noreply.github.com>
2020-04-13 23:41:26 +0900

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,23 @@
+package weekend3_2_2;
+
+import weekend3_2_2.ListNode;
+
+public class ListNode {
+	private String data;
+	public ListNode link;
+	public ListNode() {
+		this.data = null;
+		this.link = null;
+	}
+	public ListNode(String data) {
+		this.data = data;
+		this.link = null;
+	}
+	public ListNode(String data, ListNode link) {
+		this.data = data;
+		this.link = link;
+	}
+	public String getData() {
+		return this.data;
+	}
+}
